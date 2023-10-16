4e684963696131da6f059697a2ddb5e25d88005f
fdahlsko <filip.dahlskog@movial.com>
2023-10-16 21:39:09 +0300

    init booking service api call

--- client/src/service/booking.service.ts
+++ client/src/service/booking.service.ts
@@ -1,37 +1,11 @@
+import axios from "axios";
 import { Booking, BookingSearchParams } from "../interfaces";
 
-// export const getBookings = () => {
-//     return bookings;
-// }
-
-
-// export const getBookingsByDate = (date: any): Booking[] => {
-//     return bookings.filter(booking => booking.date === date);
-// }
-
-// export const getBookingsByParams = (params: BookingSearchParams): Booking[] => {
-//     const { type, name, swc_number, date, time_from, time_to } = params;
-//     const filteredBookings = bookings.filter(booking => {
-//         return booking.type === type ||
-//             booking.date === date ||
-//             booking.swc_number === swc_number ||
-//             booking.name === name;
-//     })
-//     return filteredBookings;
-// }
-
-
-// // export const getNumbersByTypeAndName = (targetType: string, targetName: string): string[] => {
-// //     const matchingNumbers: string[] = [];
-// //     // Loop through the equipment object and add numbers of the specified type and name to the array
-// //     for (const key in equipment) {
-// //         if (equipment.hasOwnProperty(key)) {
-// //             const equipmentItem = equipment[key];
-// //             if (equipmentItem.type === targetType && equipmentItem.name === targetName) {
-// //                 matchingNumbers.push(equipmentItem.swc_number);
-// //             }
-// //         }
-// //     }
-
-// //     return matchingNumbers;
-// // }
+export const getBookings = async (): Promise<Booking[] | undefined> => {
+    try {
+        const res = await axios.get('https://192.168.132.129:8000/bookings');
+        return res.data as Booking[];
+    } catch (error) {
+        console.log(error);
+    }
+}
